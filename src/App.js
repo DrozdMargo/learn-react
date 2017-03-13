eeceba89ab7632b15cb504b7382db4b911ec3c28
margo <margo@quartsoft.com>
2017-03-13 13:11:32 +0200

    state with setState

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state txt',
+            cat: 0
+        }
+    }
+
+    update(e) {
+        this.setState({txt: e.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+              <input type="text" onChange={this.update.bind(this)}/>
+              <h1>{this.state.txt}</h1>
+            </div>
+        )
+
+    }
+
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//     cat: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: 'this is the default txt'
+// }
+}
+export default App;
