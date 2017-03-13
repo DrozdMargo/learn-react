f24de64570e010bdc300e5ff9366e5950cf25b9d
margo <margo@quartsoft.com>
2017-03-13 14:18:15 +0200

    events with synthetic event system

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends React.Component {
-   render(){
-       return <Title text="123123"/>
-   }
-}
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-    text(props, propName, component){
-        if(!(propName in props)) {
-            return new Error(`missing ${propName}`)
-        }
-        if(props[propName].length <6){
-            return new Error (`${propName} was too short`)
-        }
+    constructor(){
+        super();
+        this.state = {currentEvent: '---'}
+        this.update = this.update.bind(this)
     }
+    update(e) {
+        this.setState({currentEvent: e.type})
+    }
+   render(){
+       return(
+           <div>
+               <textarea cols="30" rows="10"
+               onKeyPress={this.update}
+               onKeyDown={this.update}
+               onCut={this.update}
+               onPaste={this.update}
+               onCopy={this.update}
+               onFocus={this.update}
+               onBlur={this.update}
+               onTouchMove={this.update}
+               />
+               <h1>{this.state.currentEvent}</h1>
+           </div>
+           )
+   }
 }
 export default App;
